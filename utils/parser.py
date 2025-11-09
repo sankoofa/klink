7c351701246f80f3f226a4de60844fa910c6b6c4
cafewhaze <contaparaaescola60@gmail.com>
2025-11-09 19:56:39 -0300

    feat: Add GitHub link listener and remove fetcher.py
    The GitHubListener cog listens for GitHub links and sends a formatted
    response. The fetcher.py file was removed.

--- utils/parser.py
+++ utils/parser.py
@@ -1,25 +1,4 @@
 from discord.ext import commands
-import re
-from utils import parser
 
-class GitHubListener(commands.Cog):
-    def __init__(self, bot: commands.Bot):
-        self.bot = bot
-
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        if message.author == self.bot.user:
-            return
-            
-        match = re.search(
-            r'https?://(?:www\.)?github\.com/[\w\-\.]+/[\w\-\.]+/blob/[\w\-\/\.]+#L\d+(?:-L\d+)?',
-            message.content
-        )
-        if not match:
-            return
-        if match:
-            await message.channel.send(f'bump:\n{match.group(0)}')
-
-
-async def setup(bot):
-    await bot.add_cog(GitHubListener(bot))
+async def setup(bot: commands.Bot):
+    print("[parser.py] carregado com sucesso")
