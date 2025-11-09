a34278e51c68d4f6068ed4cfcb1f59a7670b7f92
cafewhaze <contaparaaescola60@gmail.com>
2025-11-09 19:53:48 -0300

    Add GitHub link listener
    Adds a listener that detects GitHub blob links in messages and sends a
    confirmation message.

--- utils/parser.py
+++ utils/parser.py
@@ -1,15 +1,25 @@
 from discord.ext import commands
 import re
-from urllib.parse import urlparse
+from utils import parser
 
 class GitHubListener(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-    
+
     @commands.Cog.listener()
-    async def on_messsage(self, message):
-        pass
-        
-        
+    async def on_message(self, message):
+        if message.author == self.bot.user:
+            return
+            
+        match = re.search(
+            r'https?://(?:www\.)?github\.com/[\w\-\.]+/[\w\-\.]+/blob/[\w\-\/\.]+#L\d+(?:-L\d+)?',
+            message.content
+        )
+        if not match:
+            return
+        if match:
+            await message.channel.send(f'bump:\n{match.group(0)}')
+
+
 async def setup(bot):
-    await bot.addd_cog(GitHubListener(bot))
+    await bot.add_cog(GitHubListener(bot))
